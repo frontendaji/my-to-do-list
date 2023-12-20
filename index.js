dbab7dfcf32ad329fae830843d58d540c1b64612
ajisakakamandanu <frontendaji@gmail.com>
2023-12-20 14:17:21 +0700

    update UI and update function

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
-const todo = document.getElementById('inputTodo');
-let arrayTodo = [];
-const maxTodo = 5
+const tasks = document.getElementById('input-tasks');
+let arrayTasks = [];
+const maxTasks = 5;
 
-function showTodo(){
+function addTasks() {
+    const newTasks = tasks.value.trim();
 
-    let index = arrayTodo.indexOf(arrayTodo[arrayTodo.length - 1]);
-    let number = index + 1;
-    const listTodo = document.createElement('p');
-    listTodo.textContent = number + "." + " " +arrayTodo[arrayTodo.length - 1]
-    const list = document.getElementById('list');
-    list.appendChild(listTodo);
-}
-
-function addTodo() {
-    const newTodo = todo.value.trim();
-
-    if (arrayTodo.length >= maxTodo) {
-        alert('Todo Sudah Penuh, harap hapus salah satu');
+    if (arrayTasks.length >= maxTasks) {
+        alert('Task Sudah Penuh, harap hapus salah satu');
         return;
     }
 
-    if (arrayTodo.includes(newTodo)) {
-        alert('Todo sudah ada.');
+    if (arrayTasks.includes(newTasks)) {
+        alert('Task sudah ada.');
         return;
     }
+    if (newTasks === ""){
+        alert('harap isi task terlebih dahulu');
+        return;
+    }else{
+        alert('Task berhasil di tambahkan')
+    }
 
-    arrayTodo.push(newTodo);
-    console.table(arrayTodo);
-    showTodo();
+    arrayTasks.push(newTasks);
+    console.table(arrayTasks);
 
-    todo.value = "";
+    tasks.value = "";
+    showTask()
 }
 
+function showTask(){
+    const uiElement = document.getElementById('tasks');
+    uiElement.innerHTML = "";
+
+    arrayTasks.forEach(item => {
+        const liElement = document.createElement('li');
+        
+        const imgBefore = document.createElement('img');
+        imgBefore.src = 'assets/check.png';
+        imgBefore.alt = 'Checklist';
+        liElement.appendChild(imgBefore);
+
+        const textElement = document.createElement('span');
+        textElement.textContent = item;
+        liElement.appendChild(textElement);
+
+        const imgAfter = document.createElement('img');
+        imgAfter.src = 'assets/delete.png';
+        imgAfter.alt = 'Delete';
+        liElement.appendChild(imgAfter);
+
+        uiElement.appendChild(liElement);
+    });
+}
