84951bbd1592936822dfda0eb692f431814074ce
ajisakakamandanu <frontendaji@gmail.com>
2023-12-20 16:21:24 +0700

    add delete function, and move array to new array for completed task

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 const tasks = document.getElementById('input-tasks');
+let completedTasksArray = [];
 let arrayTasks = [];
 const maxTasks = 5;
+const completed = document.getElementById('completed-tasks');
+const inclompleted = document.getElementById('incompleted-tasks');
+
+function completedTasks(){
+    const completedParagraph = document.querySelector('p[onclick="completedTasks()"]');
+    const incompletedParagraph = document.querySelector('p[onclick="incompletedTasks()"]');
+
+    completed.style.display = 'flex';
+    inclompleted.style.display = 'none';
+    showCompletedTask();
+    completedParagraph.style.color = '#F6F6F7';
+    incompletedParagraph.style.color = '#6F747A';
+}
+
+function incompletedTasks(){
+    const completedParagraph = document.querySelector('p[onclick="completedTasks()"]');
+    const incompletedParagraph = document.querySelector('p[onclick="incompletedTasks()"]');
+
+    completed.style.display = 'none';
+    inclompleted.style.display = 'block';
+    completedParagraph.style.color = '#6F747A';
+    incompletedParagraph.style.color = '#F6F6F7';
+}
 
 function addTasks() {
     const newTasks = tasks.value.trim();
 
     if (arrayTasks.length >= maxTasks) {
-        alert('Task Sudah Penuh, harap hapus salah satu');
+        alert('Task is Full, please delete one');
         return;
     }
 
     if (arrayTasks.includes(newTasks)) {
-        alert('Task sudah ada.');
+        alert('The task already exists.');
         return;
     }
     if (newTasks === ""){
-        alert('harap isi task terlebih dahulu');
+        alert('Please complete the task first');
         return;
     }else{
-        alert('Task berhasil di tambahkan')
+        alert('Task added successfully')
     }
 
     arrayTasks.push(newTasks);
@@ -40,6 +64,19 @@ function showTask(){
         imgBefore.alt = 'Checklist';
         liElement.appendChild(imgBefore);
 
+        imgBefore.onclick = function(){
+            if(completedTasksArray.length >= maxTasks){
+                alert('Completed Task is Full, Please delete one')
+                return;
+            }else{
+                completedTasksArray.push(item);
+                arrayTasks.shift(item);
+                showTask();
+            }
+            console.log(completedTasksArray);
+            console.log(arrayTasks);
+        }
+
         const textElement = document.createElement('span');
         textElement.textContent = item;
         liElement.appendChild(textElement);
@@ -48,7 +85,43 @@ function showTask(){
         imgAfter.src = 'assets/delete.png';
         imgAfter.alt = 'Delete';
         liElement.appendChild(imgAfter);
+        
+        imgAfter.onclick = function(){
+            arrayTasks.shift(item);
+            alert('The task was successfully deleted');
+            showTask();
+        };
 
         uiElement.appendChild(liElement);
     });
 }
+
+function showCompletedTask() {
+    const completedTasksList = document.getElementById('Completed');
+    completedTasksList.innerHTML = "";
+
+    completedTasksArray.forEach((task, index) => {
+        const liElement = document.createElement('li');
+
+        const imgAfter = document.createElement('img');
+        imgAfter.src = 'assets/delete.png';
+        imgAfter.alt = 'Delete';
+
+        imgAfter.onclick = function() {
+            completedTasksArray.splice(index, 1);
+            showCompletedTask();
+            alert('The task was successfully deleted');
+        };
+
+        liElement.appendChild(imgAfter);
+
+        const spanElement = document.createElement('span');
+        spanElement.textContent = `${index + 1}. ${task}`;
+        liElement.appendChild(spanElement);
+
+        completedTasksList.appendChild(liElement);
+    });
+}
+
+
+
